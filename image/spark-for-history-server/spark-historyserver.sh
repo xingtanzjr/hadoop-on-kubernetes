4293b4e9dcb0a4931f453fb57c88f5d267fa4b98
Jinrui Zhang (FA TALENT) <t-jizha@microsoft.com>
2018-04-12 18:43:15 +0800

    xx

---
+++ image/spark-for-history-server/spark-historyserver.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+hadoop fs -fs "${CORE_CONF_fs_defaultFS}" -test -e "${spark_eventLog_dir}"
+if [ $? -eq 0 ] ;then  
+    echo 'spark history dir existed'
+else  
+    hadoop fs -fs "${CORE_CONF_fs_defaultFS}" -mkdir -p "${spark_eventLog_dir}"
+    echo 'create dir for spark-history-sever: ${spark_eventLog_dir}'
+fi  
+
+exec ./sbin/start-history-server.sh "${SPARK_CONF_spark_eventLog_dir}"
