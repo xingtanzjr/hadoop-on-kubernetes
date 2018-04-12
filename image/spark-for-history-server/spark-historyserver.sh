ef538e9b1211118de6f41044f67cdcb692e03ced
xingtanzjr <xingtanzjr@163.com>
2018-04-12 21:12:37 +0800

    done

--- image/spark-for-history-server/spark-historyserver.sh
+++ image/spark-for-history-server/spark-historyserver.sh
@@ -5,9 +5,9 @@ if [ $? -eq 0 ] ;then
     echo 'spark history dir existed'
 else  
     hadoop fs -fs "${CORE_CONF_fs_defaultFS}" -mkdir -p "${spark_eventLog_dir}"
-    echo 'create dir for spark-history-sever: ${spark_eventLog_dir}'
+    echo 'create dir for spark-history-sever: "${spark_eventLog_dir}"'
 fi  
 
-exec ./sbin/start-history-server.sh "${SPARK_CONF_spark_eventLog_dir}"
-echo "start spark history server done."
-tail -f /spark-historyserver.sh
+echo "starting spark history server..."
+./bin/spark-class org.apache.spark.deploy.history.HistoryServer "${SPARK_CONF_spark_eventLog_dir}"
+# exec ./sbin/start-history-server.sh "${SPARK_CONF_spark_eventLog_dir}"
